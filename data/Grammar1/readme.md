852556bffda683f9ab5adfceb5eae453b9acdb13
Kristof Kraus <kristofkraus@gmail.com>
2023-11-09 12:33:40 +0300

    Added bidirectional LSTM model.

--- data/Grammar1/readme.md
+++ data/Grammar1/readme.md
@@ -1,2 +1,10 @@
 Check whether models can learn simple grammar: using pronouns and the correct
 conjugation of be
+
+Note that masked prediction cannot be perfect:
+
+Seeing '<MASK> are' there are three equally valid options:
+'you are', 'we are', 'they are'
+A good model should give all of them high probabilities.
+
+E.g., one model run: 'you are' (0.286), 'we are' (0.311)
--- models/get_model.py
+++ models/get_model.py
@@ -1,4 +1,5 @@
 from models.char_to_char_lstm import CharToCharLSTM
+from models.char_to_char_bi_lstm import CharToCharBiLSTM
 from text_encoding.text_encoder import TextEncoder
 
 
@@ -6,6 +7,11 @@ def get_model(text_encoder: TextEncoder, vocab_size: int, config: dict):
     match config['model']:
         case 'CharToCharLSTM':
             return CharToCharLSTM(text_encoder, vocab_size, num_neurons=config['num_neurons'],
+                                  batch_size=config['batch_size'], epochs=config['epochs'],
+                                  save_epochs=config['save_epochs'], validation_split=config['validation_split'],
+                                  learning_rate=config["learning_rate"])
+        case 'CharToCharBiLSTM':
+            return CharToCharBiLSTM(text_encoder, vocab_size, num_neurons=config['num_neurons'],
                                   batch_size=config['batch_size'], epochs=config['epochs'],
                                   save_epochs=config['save_epochs'], validation_split=config['validation_split'],
                                   learning_rate=config["learning_rate"])
