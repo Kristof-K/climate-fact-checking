31c0a13d33a17d895ac8a4b2977f3f26d00bafd6
“Kristof <kristofkraus@gmail.com>
2023-11-22 12:37:21 +0300

    Define model strucutre with multiple hidden layers.

--- models/char_to_char_lstm.py
+++ models/char_to_char_lstm.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from keras.models import Model
 from keras.layers import Input, LSTM, Dense
 from keras.optimizers import RMSprop
@@ -11,22 +13,40 @@ class CharToCharLSTM(MaskedChartoChar):
     # https://blog.keras.io/a-ten-minute-introduction-to-sequence-to-sequence-learning-in-keras.html
     # or as presented in: Lane, Hobson and Howard, Cole and Hapke, Hannes Max: Natural Language Processing in Action
 
-    def __init__(self, text_encoder: CharOneHotEncoder, num_neurons: int,
+    def __init__(self, text_encoder: CharOneHotEncoder, num_neurons: List[int],
                  batch_size: int, epochs: int, save_epochs: int, learning_rate: float):
         vocab_size = text_encoder.get_vocab_size()
         # define Network structure
         # encoder encodes input sequence of arbitrary length (dimension None!)
         encoder_in = Input(shape=(None, vocab_size))
-        encoder_out = LSTM(num_neurons, return_state=True)  # output hidden states
+
+        encoder_lstms = []
+        for i in range(1, len(num_neurons)):
+            encoder_lstms.append(LSTM(num_neurons[i - 1], return_sequences=True))
+        encoder_lstms.append(LSTM(num_neurons[-1], return_state=True))    # output hidden states
+
+        encoder_out = encoder_in
+        for lstm_l in encoder_lstms:
+            encoder_out = lstm_l(encoder_out)       # send input through the layers
+
         # we do not return sequences, hence encoder_output == state_h; and state_c is the long-term memory state
-        encoder_output, en_state_h, en_state_c = encoder_out(encoder_in)
+        encoder_output, en_state_h, en_state_c = encoder_out
         encoder_states = [en_state_h, en_state_c]
 
         # decoder gets the target sequence with start token as input and is trained to
         # reproduce the target sequence without start token
         decoder_in = Input(shape=(None, vocab_size))
-        decoder_lstm = LSTM(num_neurons, return_sequences=True, return_state=True)
-        decoder_intermed, de_state_h, de_state_c = decoder_lstm(decoder_in, initial_state=encoder_states)
+
+        decoder_lstms = []
+        for i in reversed(range(1, len(num_neurons))):
+            decoder_lstms.append(LSTM(num_neurons[i], return_sequences=True))
+        decoder_lstms.append(LSTM(num_neurons[0], return_sequences=True, return_state=True))    # output hidden states
+
+        decoder_proc = decoder_lstms[0](decoder_in, initial_state=encoder_states)      # initially we need encoder state
+        for i in range(1, len(decoder_lstms)):
+            decoder_proc = decoder_lstms[i](decoder_proc)       # send input through the layers
+
+        decoder_intermed, de_state_h, de_state_c = decoder_proc
         decoder_dense = Dense(vocab_size, activation='softmax')
         decoder_out = decoder_dense(decoder_intermed)
 
--- models/wordvec_to_word_lstm.py
+++ models/wordvec_to_word_lstm.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from keras.models import Model
 from keras.layers import Input, LSTM, Dense
 from keras.optimizers import RMSprop
@@ -10,16 +12,24 @@ class WordvecToWordLSTM(MaskedWordModel):
     # Use LSTM layers to process sentences encoded by word embeddings, and use Dense softmax
     # learn distribution over masked words
 
-    def __init__(self, text_encoder: MyWord2Vec, num_neurons: int,
+    def __init__(self, text_encoder: MyWord2Vec, num_neurons: List[int],
                  batch_size: int, epochs: int, save_epochs: int, learning_rate: float):
         # define Network structure
         # encoder encodes input sequence of arbitrary length (dimension None!)
         model_in = Input(shape=(None, text_encoder.vec_dim))
+
+        lstm_layers = []
+        for i in range(1, len(num_neurons)):
+            lstm_layers.append(LSTM(num_neurons[i - 1], return_sequences=True))
         # we cannot output sequences (arbitrary length!) as we want to process input further
-        model_lstm = LSTM(num_neurons, return_sequences=False)
+        lstm_layers.append(LSTM(num_neurons[-1], return_sequences=False))
         # just process the intermediate state to make the prediction
         model_dense = Dense(text_encoder.get_vocab_size(), activation='softmax')
-        model_out = model_dense(model_lstm(model_in))
+
+        model_out = model_in
+        for lstm_l in lstm_layers:
+            model_out = lstm_l(model_out)
+        model_out = model_dense(model_out)
 
         model = Model(inputs=model_in, outputs=model_out)
         optimizer = RMSprop(learning_rate=learning_rate)
